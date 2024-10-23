b13bd018c21dd985f124e1618352764a3cb7889f
Seeker4264 <sebashmorales@gmail.com>
2024-10-23 18:09:52 -0400

    branch test

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@
 export default async function Contact() {
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <h1 className="text-3xl">Contact</h1>
+            <h1 className="text-3xl">Contact Us</h1>
         </main>
     );
 };
